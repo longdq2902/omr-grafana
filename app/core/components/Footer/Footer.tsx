88af146cb36f4b3e2bb122f1cbabd7eb0df9551f
LongDQ <longdq@vnpt-technology.vn>
2019-08-15 10:52:21 +0700

    Init Project

---
+++ app/core/components/Footer/Footer.tsx
@@ -0,0 +1,30 @@
+﻿import React, { FC } from 'react';
+// import { Tooltip } from '@grafana/ui';
+
+interface Props {
+  appName: string;
+  buildVersion: string;
+}
+
+export const Footer: FC<Props> = React.memo(
+  ({ appName, buildVersion }) => {
+    return (
+      <footer className="footer">
+        <div className="text-center">
+          <ul>
+            <li>
+              <a href="https://grafana.com" target="_blank">
+                {appName}
+              </a>{' '}
+              <span>
+                v{buildVersion}
+              </span>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    );
+  }
+);
+
+export default Footer;
